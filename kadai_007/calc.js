a08145214b2d01014b06b42a9f9bea2e2bb2c3c1
mariko uehara <ms0913mp@gmail.com>
2025-04-24 01:36:06 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -3,10 +3,10 @@ let num = 15;
 else if (num % 3 === 0 && num % 5 === 0) {
   console.log('3と5の倍数です');
 } 
-if (num % 3 === 0) {
+else if (num % 3 === 0) {
   console.log('3の倍数です');
 } 
-if (num % 5 === 0) {
+else if (num % 5 === 0) {
   console.log('5の倍数です');
 } 
 else {
